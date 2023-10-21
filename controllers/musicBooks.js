fcafb11208cff29e9341b17a8a3d787bc88dd8a7
Julie Spackman <spack84@byui.edu>
2023-10-20 22:12:35 -0600

    Wk06 Personal Project, fixing DELETE 200

--- controllers/musicBooks.js
+++ controllers/musicBooks.js
@@ -85,7 +85,7 @@ const deleteBook = async (req, res) => {
   const response = await mongodb.getDb().db().collection('musicBooks').remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the musicBook.');
   }
--- controllers/students.js
+++ controllers/students.js
@@ -93,7 +93,7 @@ const deleteStudent = async (req, res) => {
   const response = await mongodb.getDb().db().collection('students').remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the student.');
   }
