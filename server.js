2d1953586a84cfed3c3829a0c0e9b06e27a5843d
Julie Spackman <spack84@byui.edu>
2023-10-25 22:51:08 -0600

    Wk7 Resetting Project 2

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-// const axios = require('axios');
 
 const port = process.env.PORT || 8080;
-// const path = require('path');
 
 const app = express();
 
 app
-  // .use(express.static('static'))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,34 +14,6 @@ app
   })
   .use('/', require('./routes'))
   
-
-  // .get('/', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '/static/index.html'));
-  // })
-  // .get('/auth', (req, res) => {
-  //   res.redirect(
-  //     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
-  //   );
-  // })
-  // .get('/oauth-callback', ({ query: { code } }, res) => {
-  //   const body = {
-  //     client_id: process.env.GITHUB_CLIENT_ID,
-  //     client_secret: process.env.GITHUB_SECRET,
-  //     code
-  //   };
-  //   const opts = { headers: { accept: 'application/json' } };
-  //   axios
-  //     .post('https://github.com/login/oauth/access_token', body, opts)
-  //     .then((_res) => _res.data.access_token)
-  //     .then((token) => {
-  //       // eslint-disable-next-line no-console
-  //       console.log('My token:', token);
-
-  //       res.redirect(`/?token=${token}`);
-  //     })
-  //     .catch((err) => res.status(500).json({ err: err.message }));
-  // });  
-
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
 });
