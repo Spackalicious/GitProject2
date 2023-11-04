e822c686af7a8cf7b4d05e20abe19f0f69755e56
Julie Spackman <spack84@byui.edu>
2023-11-03 22:51:21 -0600

    Wk08 Personal Assignment part 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,42 @@
+const { auth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config;
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SESSION_SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./swagger'));
-router.use('/musicBooks', require('./musicBooks'));
-router.use('/students', require('./students'));
+// auth router attaches /login, /logout, and /callback routes to base url
+router.use(auth(config));
+router.get('/checkLoginStatus', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// get the user profile information??
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+// router.get('/musicBooks', requiresAuth(), (req, res) => {
+//     res.send('/musicBooks', require('./musicBooks'))
+// });
+router.use('/', requiresAuth(), require('./swagger'));
+router.use('/musicBooks', requiresAuth(), require('./musicBooks'));
+router.use('/students', requiresAuth(), require('./students'));
+
+
+
+// router.use('/', require('./swagger'));
+// router.use('/musicBooks', require('./musicBooks'));
+// router.use('/students', require('./students'));
 
 module.exports = router;
